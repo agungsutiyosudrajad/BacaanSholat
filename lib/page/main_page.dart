66a43ba03852ce83ef073e8ef2a6234261593137
agungsutiyosudrajad <agungsutiyosudrajad@gmail.com>
2023-08-14 18:40:16 +0700

    commit semua file #1

---
+++ lib/page/main_page.dart
@@ -0,0 +1,120 @@
+import 'package:bacaan_sholat/page/ayat_kursi_page.dart';
+import 'package:bacaan_sholat/page/bacaan_shalat_page.dart';
+import 'package:bacaan_sholat/page/niat_shalat_page.dart';
+import 'package:bacaan_sholat/page/niat_shalat_page.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 248, 250, 249),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.all(10),
+                child: Expanded(
+                  child: InkWell(
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => NiatSholat()));
+                    },
+                    child: Column(
+                      children: [
+                        Image(
+                          image: AssetImage("assets/images/ic_niatt.png"),
+                          height: 100,
+                          width: 100,
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "Niat Sholat",
+                          style: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 40),
+              Container(
+                margin: EdgeInsets.all(10),
+                child: Expanded(
+                  child: InkWell(
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => BacaanSholat()));
+                    },
+                    child: Column(
+                      children: [
+                        Image(
+                          image: AssetImage("assets/images/ic_doaa.png"),
+                          height: 100,
+                          width: 100,
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "Bacaan Sholat",
+                          style: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 40),
+              Container(
+                margin: EdgeInsets.all(10),
+                child: Expanded(
+                  child: InkWell(
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => AyatKursi())); 
+                    },
+                    child: Column(
+                      children: [
+                        Image(
+                          image: AssetImage("assets/images/ic_bacaann.png"),
+                          height: 100,
+                          width: 100,
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "Ayat Kursi",
+                          style: TextStyle(
+                              fontSize: 14, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
